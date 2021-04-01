60a657e11567dcde62a0ad94e10aa425c3845b99
Siddharth Diwan <sidwan02@gmail.com>
2021-03-31 22:52:58 -0400

    convert response -> String w/ working Set<String> synonyms

--- server/src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/wordnikAPIHandler/WordnikAPIHandler.java
+++ server/src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/wordnikAPIHandler/WordnikAPIHandler.java
@@ -1,5 +1,6 @@
 package edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.wordnikAPIHandler;
 
+import com.google.common.reflect.TypeToken;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
@@ -8,8 +9,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EventObject;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WordnikAPIHandler {
   public Set<String> getSynonyms(String word) {
@@ -18,14 +24,28 @@ public class WordnikAPIHandler {
     HttpEntity<String> entity = new HttpEntity<>("parameters", header);
     RestTemplate rt = new RestTemplate();
 
-    ResponseEntity<WordSynonym> response = rt.exchange("https://api.wordnik.com/v4/word.json/earth/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=70538348db6b42e43a5181e32070feebc0b303e293ed13a97",
-      HttpMethod.GET, entity, WordSynonym.class);
+//    // https://docs.spring.io/spring-android/docs/current/reference/html/rest-template.html
+//    ResponseEntity<Event[]> response = rt.exchange("https://api.wordnik.com/v4/word.json/earth/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=70538348db6b42e43a5181e32070feebc0b303e293ed13a97",
+//      HttpMethod.GET, entity, Event[].class);
+//
+//    Event[] eventBody = response.getBody();
 
-    WordSynonym synonyms = response.getBody();
+    // https://docs.spring.io/spring-android/docs/current/reference/html/rest-template.html
+    ResponseEntity<String> response = rt.exchange("https://api.wordnik.com/v4/word.json/earth/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=70538348db6b42e43a5181e32070feebc0b303e293ed13a97",
+      HttpMethod.GET, entity, String.class);
 
-    assert synonyms != null;
-    System.out.println("ha: " + synonyms.getSynonyms());
+    String eventBody = response.getBody();
+//    System.out.println(eventBody);
 
-    return synonyms.getSynonyms();
+    String spliceToStart = eventBody.split("\\[", 3)[2];
+    String spliceUntilEnd = spliceToStart.split("\\]", 2)[0];
+//    System.out.println(spliceUntilEnd);
+
+    Set<String> synonyms
+      = Arrays.stream(spliceUntilEnd.split(",")).collect(Collectors.toSet());
+
+    System.out.println(synonyms);
+
+    return synonyms;
   }
 }
