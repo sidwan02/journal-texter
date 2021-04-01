feb91a14a87c9955d37657523c9a81d948c8ac72
Siddharth Diwan <sidwan02@gmail.com>
2021-04-01 10:54:52 -0400

    added private config.properties + PropertiesReader cass for API keys

--- server/src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/APIHandlers/wordSyonyms/WordnikAPIHandler.java
+++ server/src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/APIHandlers/wordSyonyms/WordnikAPIHandler.java
@@ -1,5 +1,6 @@
 package edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.APIHandlers.wordSyonyms;
 
+import edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.APIHandlers.PropertiesReader;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -17,14 +18,8 @@ public class WordnikAPIHandler {
     HttpEntity<String> entity = new HttpEntity<>("parameters", header);
     RestTemplate rt = new RestTemplate();
 
-//    // https://docs.spring.io/spring-android/docs/current/reference/html/rest-template.html
-//    ResponseEntity<Event[]> response = rt.exchange("https://api.wordnik.com/v4/word.json/earth/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=70538348db6b42e43a5181e32070feebc0b303e293ed13a97",
-//      HttpMethod.GET, entity, Event[].class);
-//
-//    Event[] eventBody = response.getBody();
-
     // https://docs.spring.io/spring-android/docs/current/reference/html/rest-template.html
-    ResponseEntity<String> response = rt.exchange("https://api.wordnik.com/v4/word.json/" + word + "/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=70538348db6b42e43a5181e32070feebc0b303e293ed13a97",
+    ResponseEntity<String> response = rt.exchange("https://api.wordnik.com/v4/word.json/" + word + "/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=" + PropertiesReader.getProperty("WORDNIK_KEY"),
       HttpMethod.GET, entity, String.class);
 
     String eventBody = response.getBody();
