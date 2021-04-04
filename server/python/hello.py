aa6465898bab6603d9920fa663bb4c9fa22a9960
Siddharth Diwan <sidwan02@gmail.com>
2021-04-03 22:47:13 -0400

    created map of phrases and sentiments

--- server/python/hello.py
+++ server/python/hello.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # class Hello:
 # #     __gui = None
 # #
@@ -16,25 +16,77 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
 import re
+import os
+
+# my_path = os.path.join(os.path.dirname(__file__), "..")
+# print(my_path)
 
 
 spice = en_core_web_sm.load()
 
+# https://towardsdatascience.com/sentiment-analysis-using-lstm-step-by-step-50d074f09948
+
 
 def run():
-    with open('data\stanfordSentimentTreebank\dictionary.txt', 'r') as f:
+    with open(
+        r"C:\Users\sidwa\OneDrive\OneDriveNew\Personal\Sid\Brown University\Courses\Computer Science\CSCI 0320\Assignments\term-project-rfameli1-sdiwan2-tfernan4-tzaw\data\stanfordSentimentTreebank\dictionary.txt",
+        "r",
+    ) as f:
         reviews = f.read()
-    with open('data\stanfordSentimentTreebank\sentiment_labels.txt', 'r') as f:
+    with open(
+        r"C:\Users\sidwa\OneDrive\OneDriveNew\Personal\Sid\Brown University\Courses\Computer Science\CSCI 0320\Assignments\term-project-rfameli1-sdiwan2-tfernan4-tzaw\data\stanfordSentimentTreebank\sentiment_labels.txt",
+        "r",
+    ) as f:
         labels = f.read()
 
     # convert into dict of phrase ID | phrase
-    # phrase_dict = {}
-    # reviews_split = reviews.split('\n')
-    # for (review in reviews_split):
-    #     phrase_components = reviews.split('\n')
-    #     phrase_dict
+    phrases_dict = {}
 
-    # print(reviews_split)
+    reviews_split = reviews.split("\n")
+    # print(reviews[:50])
+    # print(reviews_split[4])
+    for rev in reviews_split:
+        # print("review: ", review)
+        phrase_components = rev.split("|")
+        # print(phrase_components)
+        try:
+            index = phrase_components[1]
+            phrase = phrase_components[0]
+            # processed_phrase = clean(filter(lemma(phrase)))
+            processed_phrase = phrase
+            # print("phrase: ", phrase)
+            # print("processed_phrase: ", processed_phrase)
+            phrases_dict[index] = processed_phrase
+        except IndexError:
+            # this is the newline at the end of the txt
+            break
+
+    # print(phrases_dict[10])
+
+    sentiment_dict = {}
+
+    sentiment_split = labels.split("\n")
+    # print(reviews[:50])
+    # print(reviews_split[4])
+    for lab in sentiment_split:
+        # print("review: ", review)
+        sentiment_components = lab.split("|")
+        # print(phrase_components)
+        try:
+            index = sentiment_components[0]
+            sentiment = sentiment_components[1]
+            sentiment_dict[index] = sentiment
+        except IndexError:
+            # this is the newline at the end of the txt
+            break
+    # print(sentiment_dict[10])
+
+    sentiment_map = {}
+
+    for key in phrases_dict.keys():
+        sentiment_map[phrases_dict[key]] = sentiment_dict[key]
+
+    print(sentiment_map["the"])
 
 
 def clean(sentence):
@@ -42,32 +94,42 @@ def clean(sentence):
     # p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.HASHTAG, p.OPT.NUMBER)
     # sentence = p.clean(sentence)
     # replace consecutive non-ASCII characters with a space
-    sentence = re.sub(r'[^\x00-\x7F]+', ' ', sentence)
+    sentence = re.sub(r"[^\x00-\x7F]+", " ", sentence)
     # take care of contractions and stray quote marks
-    sentence = re.sub(r'’', "'", sentence)
+    sentence = re.sub(r"’", "'", sentence)
     # words = sentence.split()
     sentence = re.sub(r":", " ", sentence)
     sentence = re.sub(r"n't", " not", sentence)
     # fix spellings
-    sentence = ''.join(''.join(s)[:2] for _, s in itertools.groupby(sentence))
+    sentence = "".join("".join(s)[:2] for _, s in itertools.groupby(sentence))
     # emojis conversion
     sentence = emoji.demojize(sentence)
-    sentence = ' '.join(sentence.split())
+    sentence = " ".join(sentence.split())
     return sentence
 
 
 def filter(sentence):
-    stop_words = set(stopwords.words('english'))
-    stray_tokens = ['amp', '`', "``", "'", "''",
-                    '"', "n't", "I", "i", ",00"]  # stray words
-    punct = r'[{}]'.format(string.punctuation)
-    sentence = re.sub(punct, ' ', sentence)
-    sentence = re.sub(r'[0-9]', ' ', sentence)
+    stop_words = set(stopwords.words("english"))
+    stray_tokens = [
+        "amp",
+        "`",
+        "``",
+        "'",
+        "''",
+        '"',
+        "n't",
+        "I",
+        "i",
+        ",00",
+    ]  # stray words
+    punct = r"[{}]".format(string.punctuation)
+    sentence = re.sub(punct, " ", sentence)
+    sentence = re.sub(r"[0-9]", " ", sentence)
     # # correct spelling mistakes
     # sentence = re.sub(
     #     r'privacy|privcy|privac|privasy|privasee|privarcy|priavcy|privsy', ' privacy ', sentence)
     # sentence = re.sub(r'(^|\s)[a-z]($|\s)', ' ', sentence)
-    sentence = re.sub(r'(^|\s)[a-z][a-z]($|\s)', ' ', sentence)
+    sentence = re.sub(r"(^|\s)[a-z][a-z]($|\s)", " ", sentence)
     word_tokens = word_tokenize(sentence)
     # filter using NLTK library append it to a string
     filtered_sentence = [w for w in word_tokens if not w in stop_words]
@@ -75,21 +137,24 @@ def filter(sentence):
     # looping through conditions
     for w in word_tokens:
         # check tokens against stopwords and punctuations
-        if w not in stop_words and w not in string.punctuation and w not in stray_tokens:
+        if (
+            w not in stop_words
+            and w not in string.punctuation
+            and w not in stray_tokens
+        ):
             w = w.lower()
             filtered_sentence.append(w)
-    sentence = ' '.join(filtered_sentence)
+    sentence = " ".join(filtered_sentence)
     # re-removing single characters
-    sentence = re.sub(r'(^|\s)[a-z]($|\s)', ' ', sentence)
-    sentence = re.sub(r'(^|\s)[a][a]($|\s)', ' ',
-                      sentence)  # fixing for privacy
+    sentence = re.sub(r"(^|\s)[a-z]($|\s)", " ", sentence)
+    sentence = re.sub(r"(^|\s)[a][a]($|\s)", " ", sentence)  # fixing for privacy
     return sentence
 
 
 def lemma(sentence):
     doc = spice(sentence)
-    wrong_class = ['pending', 'madras', 'data', 'media']
-    wrong_words = ' '.join(wrong_class)
+    wrong_class = ["pending", "madras", "data", "media"]
+    wrong_words = " ".join(wrong_class)
     wrong_doc = spice(wrong_words)
     lemma_sentence = []
     for token in doc:
@@ -97,12 +162,20 @@ def lemma(sentence):
             token = token.lemma_
             if token != "-PRON-":
                 lemma_sentence.append(token)
-    return ' '.join(lemma_sentence)
+    return " ".join(lemma_sentence)
 
 
-print(lemma(filter(clean("The camera twirls ! Oh, look at that clever angle ! Wow, a jump cut !"))))
+# print(
+#     lemma(
+#         filter(
+#             clean(
+#                 "The camera twirls ! Oh, look at that clever angle ! Wow, a jump cut !"
+#             )
+#         )
+#     )
+# )
 
-# run()
+run()
 
 
 def run2(something):
