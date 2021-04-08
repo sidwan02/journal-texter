7bc1166a3e4e698d9ec93befbc77430a5da27bbb
Siddharth Diwan <sidwan02@gmail.com>
2021-04-08 10:19:34 -0400

    modified Python interpreter to use Buffered Reader instead of jython

--- server/java/src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/pythonConnection/PythonInterpreter.java
+++ server/java/src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/pythonConnection/PythonInterpreter.java
@@ -1,66 +1,28 @@
-package edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.pythonConnection;
-// https://stackoverflow.com/questions/16460468/can-we-call-a-python-method-from-java
-import org.python.core.PyInstance;
+package edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.pythonConnection;//package edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.pythonConnection;
+//// https://stackoverflow.com/questions/16460468/can-we-call-a-python-method-from-java
+//    // https://stackoverflow.com/questions/9381906/how-to-call-a-python-method-from-a-java-class
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-
-public class PythonInterpreter
-{
-
-  public org.python.util.PythonInterpreter interpreter;
-
-  public PythonInterpreter()
-  {
-    org.python.util.PythonInterpreter.initialize(System.getProperties(),
-      System.getProperties(), new String[0]);
-
-    this.interpreter = new org.python.util.PythonInterpreter();
-  }
-
-  void execfile( final String fileName )
-  {
-    this.interpreter.execfile(fileName);
-  }
-
-  PyInstance createClass( final String className, final String opts )
-  {
-    return (PyInstance) this.interpreter.eval(className + "(" + opts + ")");
-  }
-
-  public static void main(String[] args)
-  {
-    PythonInterpreter ie = new PythonInterpreter();
-
-    System.out.println("hi");
-
-//    PyInstance hello = ie.createClass("Hello", "None");
-
-//    int something = 10;
-
-    HashMap<Integer, String> map = new HashMap<>();
-    map.put(0, "hi");
-
-//    ie.interpreter.exec("import en_core_web_sm");
-//    ie.interpreter.exec("import numpy as np");
-//    ie.interpreter.exec("import spacy");
-//    ie.interpreter.exec("import itertools");
-//    ie.interpreter.exec("import emoji");
-//    ie.interpreter.exec("from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer");
-//    ie.interpreter.exec("from nltk.tokenize import word_tokenize");
-//    ie.interpreter.exec("from nltk.corpus import stopwords");
-//    ie.interpreter.exec("import string");
-//    ie.interpreter.exec("import re");
-
-    ie.execfile("server/python/hello.py");
-
-    ie.interpreter.set("something", map);
-//    PyObject pyResult = ie.interpreter.get("hello");
-
-    // for invoke -> http://web.mit.edu/jython/jythonRelease_2_2alpha1/Doc/javadoc/org/python/core/PyInstance.html
-//    hello.invoke("run");
-    // https://stackoverflow.com/questions/9381906/how-to-call-a-python-method-from-a-java-class
-//    ie.interpreter.eval("repr(Hello().run(something))");
-    ie.interpreter.eval("repr(run2(something))");
+public class PythonInterpreter {
+  public static void main(String[] args) {
+//    try(PythonInterpreter pyInterp = new PythonInterpreter()) {
+//      pyInterp.exec("print('Hello Python World!')");
+//    }
+    try {
+      Process p = Runtime.getRuntime().exec("python server/python/predict.py");
+      BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String ret = null;
+      while ((ret = in.readLine()) != null) {
+        System.out.println(ret);
+      }
+//      System.out.println("ret);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
