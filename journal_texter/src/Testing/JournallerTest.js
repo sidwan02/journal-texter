53cd8c7133b18df2aadf12bac9200949f93f5957
Theo Fernandez <theofernandez@theos-mbp.devices.brown.edu>
2021-04-15 18:31:35 -0400

    Questions bar now formatted in react journaller page

--- journal_texter/src/Testing/JournallerTest.js
+++ journal_texter/src/Testing/JournallerTest.js
@@ -1,12 +1,45 @@
 import './JournallerTest.css';
 import NavBar from "../dashboard/NavBar";
 import React, {useState} from "react";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 
 export default function () {
     const [userResponse, setUserResponse] = useState("");
 
     function test() {
-        alert("It works");
+        submitUserForm();
+    }
+
+    function enterPressed(event) {
+        let code = event.keyCode || event.which;
+        if(code === 13) {
+            submitUserForm();
+        }
+    }
+
+    function submitUserForm() {
+        setQuestion1("test1");
+        setQuestion2("test2");
+        setQuestion3("test3");
+        setQuestion4("test4");
+        setQuestion5("test5");
+    }
+
+    const [selectedQuestion, setSelectedQuestion] = useState("");
+    const [question1, setQuestion1] = useState("test1");
+    const [question2, setQuestion2] = useState("test2");
+    const [question3, setQuestion3] = useState("test3");
+    const [question4, setQuestion4] = useState("test4");
+    const [question5, setQuestion5] = useState("test5");
+
+    const handleQuestions = (event, newQuestion) => {
+        setSelectedQuestion(newQuestion);
+        setQuestion1("");
+        setQuestion2("");
+        setQuestion3("");
+        setQuestion4("");
+        setQuestion5("");
     }
 
     return (
@@ -14,16 +47,33 @@ export default function () {
             <NavBar/>
             <div className="journaller-body">
                 <div className="text-display grid-element"></div>
-                <div className="question-display grid-element"></div>
-                <div className="user-input grid-element">
-                    <form className="user-input-form">
-                        <input type="text" className="journal-response"/>
-                        <input type="submit" onClick={test} className="journal-submit-button"/>
-                    </form>
+                <div className="question-display grid-element">
+                    <div className="question-display-header question-display-element">
+                        <div className="question-display-header-text">
+                            Select a prompt to respond to!
+                        </div>
+                    </div>
+                    <div className="prompt-selector question-display-element">
+                        <div>{question1}</div>
+                    </div>
+                    <div className="prompt-selector question-display-element">
+                        <div>{question2}</div>
+                    </div>
+                    <div className="prompt-selector question-display-element">
+                        <div>{question3}</div>
+                    </div>
+                    <div className="prompt-selector question-display-element">
+                        <div>{question4}</div>
+                    </div>
+                    <div className="prompt-selector question-display-element">
+                        <div>{question5}</div>
+                    </div>
                 </div>
-                <div className="save-journal-element grid-element">
-                    <button className="save-journal-entry" onClick={test}>Save</button>
-                </div>
+                <input type="text" onKeyPress={enterPressed}
+                       onChange={event => setUserResponse(event.target.value)}
+                       className="grid-element journal-type-box"/>
+                <button onClick={test} className="grid-element text-submit-button journal-button">Send</button>
+                <button onClick={test} className="save-journal-entry grid-element journal-button">Save</button>
             </div>
         </div>
     );
