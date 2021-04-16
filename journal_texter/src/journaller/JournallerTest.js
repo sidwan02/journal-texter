6faf50209c873956ccd29f0ce0a85dc5e2f67654
thhtzaw <thet_htay_zaw@brown.edu>
2021-04-15 23:45:02 -0400

    Successfully moved to new interface

--- journal_texter/src/journaller/JournallerTest.js
+++ journal_texter/src/journaller/JournallerTest.js
@@ -1,27 +1,120 @@
 import '../css/JournallerTest.css';
 import NavBar from "../dashboard/NavBar";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import QuestionDisplay from "./SubComponents/QuestionDisplay";
 import HiddenQuestionDisplay from "./SubComponents/HiddenQuestionDisplay";
+import axios from "axios";
 
-export default function () {
+export default function (props) {
     const [userResponse, setUserResponse] = useState("");
     const [texts, setTexts] = useState([]);
+    const [recentUserResponse, setRecentUserResponse] = useState([]);
     const [showQuestionDisplay, setShowQuestionDisplay] = useState(true);
     // const [questions, setQuestions] = useState(["", "", "", "", ""]);
     // for testing
-    const [questions, setQuestions] = useState(["what happens if the question is longer and therefore takes up more space?", "prompt 2", "prompt 3", "prompt 4", "prompt 5"]);
+    const [questions, setQuestions] = useState(["", "", "", "", ""]);
+    const user = JSON.parse(localStorage.getItem('token'))['token'];
+    const entryID = props.location.state.entryID;
 
+    /**
+     * Loads in a random question at the start
+     */
+    const firstQuestionLoad = async () => {
+        const toSend = {
+            entryID: entryID,
+            userID: user,
+            text: [],
+            state: "start"
+        }
+
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+        axios.post(
+            "http://localhost:4567/handleRequestQuestion",
+            toSend,
+            config
+        ).then(response => {
+            let questionsList = response.data["questions"]
+            let firstQuestion = questionsList[0];
+            setTexts(texts.concat(
+                <div className="journal-entry-text-container align-left">
+                    <div className="journal-entry-text">{firstQuestion}</div>
+                </div>));
+
+        })
+    }
+
+    useEffect(() => {
+        firstQuestionLoad()
+    }, [])
+
+    /**
+     * Updates the journal with user input. Sends a POST request to the backend to generate new questions
+     */
     const addResponse = () => {
         setTexts(texts.concat(
             <div className="journal-entry-text-container align-right">
                 <div className="journal-entry-text">{userResponse}</div>
             </div>));
+
+        setRecentUserResponse(recentUserResponse.concat(userResponse));
+
+        const toSend = {
+            entryID: entryID,
+            userID: user,
+            text: recentUserResponse,
+            state: "requestQuestion"
+        }
+
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+        axios.post(
+            "http://localhost:4567/handleRequestQuestion",
+            toSend,
+            config
+        ).then(response => {
+            let questionsList = response.data["questions"]
+            setQuestions(questionsList);
+        })
     }
 
+    /**
+     * Manually saves the entry
+     */
     function saveEntry() {
-        // TODO Connect to save functionality
-        alert("NEED TO ADD SAVE ENTRY");
+        const toSend = {
+            entryID: entryID,
+            question: "",
+            userID: user,
+            text: recentUserResponse,
+            state: "saveEntry"
+        }
+
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+        axios.post(
+            "http://localhost:4567/handleSaveUserInputs",
+            toSend,
+            config
+        ).then(response => {
+
+        })
+        //alert("NEED TO ADD SAVE ENTRY");
     }
 
     function enterPressed(event) {
@@ -48,6 +141,9 @@ export default function () {
                 </div>
                 {showQuestionDisplay ?
                     <QuestionDisplay questions={questions} texts={texts} setTexts={setTexts}
+                                     recentUserResponse={recentUserResponse}
+                                     setRecentUserResponse={setRecentUserResponse}
+                                     user={user} entryID={entryID}
                                                         setShowQuestionDisplay={setShowQuestionDisplay}/>
                                                         : <HiddenQuestionDisplay />}
                 <input type="text" onKeyPress={enterPressed}
