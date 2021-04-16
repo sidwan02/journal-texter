153377fa479545a2d497efb76ea0093b9be391b6
Siddharth Diwan <sidwan02@gmail.com>
2021-04-16 08:33:39 -0400

    completed documentation GUIHander non login methods + classes

--- src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/postRequestHandler/GUIHandler.java
+++ src/main/java/edu/brown/cs/rfameli1_sdiwan2_tfernan4_tzaw/postRequestHandler/GUIHandler.java
@@ -24,16 +24,18 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * postRequestHandler class to manage all handlers to the stars page.
+ * postRequestHandler class to manage all handlers to the JournalTexter Page.
  */
 public class GUIHandler {
 
+  /**
+   * Class that manages all requests for new questions.
+   */
   public static class HandleRequestQuestion implements Route {
     private static final Gson GSON = new Gson();
 
     /**
-     * Handles Axios requests from the javascript front-end and returns
-     * the appropriate JSON object to be used by the front-end.
+     * Handles request to send a new batch of questions to the frontend.
      *
      * @param request  - request object for Axios request
      * @param response - response object for Axios request
@@ -42,17 +44,11 @@ public class GUIHandler {
      */
     @Override
     public Object handle(Request request, Response response) throws Exception {
-      // If state requestQuestions Saves previous response list and new selected question, entry tags created here
-      // If state start just gives questions
-
       JSONObject data = new JSONObject(request.body());
       Map<String, Object> variables;
 
       // id of the entry
       Integer entryId = Integer.parseInt(data.getString("entryID"));
-
-      // username
-      String username = data.getString("userID");
       // list of responses in the from of a JSON array
       JSONArray text = data.getJSONArray("text");
       // either "start" or "requestQuestion"
@@ -68,13 +64,15 @@ public class GUIHandler {
 
         JournalTexterDB jtDB = JournalTexterDB.getInstance();
 
+        // save the question
         jtDB.addToEntry(entryId, entryInfo);
 
         variables = ImmutableMap.of(
             "questions", questions,
-            "tags", new ArrayList<>(),
-            "sentiment", -1);
+            "tags", new ArrayList<>(), // no responses data
+            "sentiment", -1); // no responses data
       } else if (state.equals("requestQuestion")) {
+        // convert the JSONArray
         List<String> responses = new ArrayList<>();
         if (text != null) {
           int len = text.length();
@@ -89,6 +87,7 @@ public class GUIHandler {
 
         List<String> questions = BackendConnection.getQuestionsFromTags(foundTags);
 
+        // in case not enough questions are determined from tags
         List<String> additionalQuestions
             = BackendConnection.getRandomlyGeneratedQuestions(5 - questions.size());
 
@@ -103,30 +102,25 @@ public class GUIHandler {
             "tags", foundTags,
             "sentiment", sentiment);
       } else {
+        // should not be entering this state
         variables = ImmutableMap.of(
             "questions", new ArrayList<>(),
             "tags", new ArrayList<>(),
             "sentiment", -1);
       }
 
-      /*-------*/
-      /*
-      You can get all the tags in the database using the jtDatabase getAllTagsFromDb method and
-      find which ones apply using your vectorizor. Then you can find all questions that use those
-      tags using findQuestionsFromTag (returns a List of Questions).
-       */
-      /*-------*/
-
       return GSON.toJson(variables);
     }
   }
 
+  /**
+   * Class that manages all requests to save user inputs.
+   */
   public static class HandleSaveUserInputs implements Route {
     private static final Gson GSON = new Gson();
 
     /**
-     * Handles Axios requests from the javascript front-end and returns
-     * the appropriate JSON object to be used by the front-end.
+     * Handles request to save data either from clicking on a new question or saving the entry.
      *
      * @param request  - request object for Axios request
      * @param response - response object for Axios request
@@ -135,9 +129,6 @@ public class GUIHandler {
      */
     @Override
     public Object handle(Request request, Response response) throws Exception {
-      // If state requestQuestions Saves previous response list and new selected question, entry tags created here
-      // If state start just gives questions
-
       JSONObject data = new JSONObject(request.body());
       Map<String, Object> variables;
 
@@ -145,16 +136,15 @@ public class GUIHandler {
       Integer entryId = Integer.parseInt(data.getString("entryID"));
       // question selected by user
       String question = data.getString("question");
-      // username
-      String username = data.getString("userID");
       // list of responses in the form of a JSON array
       JSONArray text = data.getJSONArray("text");
       // either "saveQuestion" or "saveEntry"
       // "saveQuestion" -> after the user clicks on a new question
-      // "saveEntry" -> after the user clicks on the save entry btn
+      // "saveEntry" -> after the user clicks on the save btn
       String state = data.getString("state");
 
       if (state.equals("saveQuestion")) {
+        // convert the JSONArray
         List<String> responses = new ArrayList<>();
         if (text != null) {
           int len = text.length();
@@ -170,11 +160,13 @@ public class GUIHandler {
         double sentiment = -1.0;
         //double sentiment = BackendConnection.getSentimentFromResponses(combinedResponses);
 
+        // save the responses
         List<JournalText> entryInfo = new ArrayList<>();
         for (String res : responses) {
           entryInfo.add(new edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.Journal.Response(res));
         }
 
+        // save the question
         entryInfo.add(new Question(question));
 
         JournalTexterDB jtDB = JournalTexterDB.getInstance();
@@ -184,25 +176,8 @@ public class GUIHandler {
         variables = ImmutableMap.of(
           "tags", foundTags,
           "sentiment", sentiment);
-//        List<String> responses = new ArrayList<>();
-//        if (text != null) {
-//          int len = text.length();
-//          for (int i = 0; i < len; i++) {
-//            responses.add(text.get(i).toString());
-//          }
-//        }
-//
-//        String combinedResponses = String.join(" ", responses);
-//
-//        Set<String> foundTags = BackendConnection.getTagsFromResponses(combinedResponses);
-//
-//        double sentiment = -1.0;
-//        //double sentiment = BackendConnection.getSentimentFromResponses(combinedResponses);
-//
-//        variables = ImmutableMap.of(
-//            "tags", foundTags,
-//            "sentiment", sentiment);
       } else if (state.equals("saveEntry")) {
+        // convert JSONArray
         List<String> responses = new ArrayList<>();
         if (text != null) {
           int len = text.length();
@@ -218,12 +193,11 @@ public class GUIHandler {
         double sentiment = -1.0;
         //double sentiment = BackendConnection.getSentimentFromResponses(combinedResponses);
 
+        // save the responses
         List<JournalText> entryInfo = new ArrayList<>();
         for (String res : responses) {
           entryInfo.add(new edu.brown.cs.rfameli1_sdiwan2_tfernan4_tzaw.Journal.Response(res));
         }
-
-//        entryInfo.add(new Question(question));
 
         JournalTexterDB jtDB = JournalTexterDB.getInstance();
 
@@ -233,54 +207,44 @@ public class GUIHandler {
             "tags", foundTags,
             "sentiment", sentiment);
       } else {
+        // should not reach this branch
         variables = ImmutableMap.of(
             "questions", new ArrayList<>(),
             "tags", new ArrayList<>(),
             "sentiment", -1);
       }
 
-      /*-------*/
-      /*
-      You can get all the tags in the database using the jtDatabase getAllTagsFromDb method and
-      find which ones apply using your vectorizor. Then you can find all questions that use those
-      tags using findQuestionsFromTag (returns a List of Questions).
-       */
-      /*-------*/
-
-      /*-------*/
-      /*
-      When a user selects a question, that question should be added to the entry string. This would
-      essentially be updating the entry in the database, and would have similar functionality
-      to HandleSaveUserEntry. I can also easily create an appendToCurrentEntry method to add to
-      the entry. This would require the entry's id, the username, and the text to be appended.
-       */
-      /*-------*/
-
       return GSON.toJson(variables);
     }
   }
 
+  /**
+   * Class that manages all requests to create a new entry.
+   */
   public static class HandleCreateEntry implements Route {
     private static final Gson GSON = new Gson();
 
-    //     Request ==>
-//     Response ==> id of the entry that was created, store on the frontend and use it to request
-//     specific entries from the backend
+    /**
+     * Handles request to create a new entry.
+     *
+     * @param request  - request object for Axios request
+     * @param response - response object for Axios request
+     * @return a JSON object representing information to be used by the front end
+     * @throws Exception if data cannot be accessed from given JSON object
+     */
     @Override
     public Object handle(Request request, Response response) throws Exception {
-      // If state requestQuestions Saves previous response list and new selected question, entry tags created here
-      // If state start just gives questions
-
       JSONObject data = new JSONObject(request.body());
       Map<String, Object> variables;
 
-      String userNameOrUserID = data.getString("userID");
-      // String state = data.getString("state");
+      String username = data.getString("userID");
 
       JournalTexterDB jtDB = JournalTexterDB.getInstance();
 
-      int entryId = jtDB.addUserEntry(LocalDate.now(), "", userNameOrUserID);
+      // create a blank entry
+      int entryId = jtDB.addUserEntry(LocalDate.now(), "", username);
 
+      // give the entry ID for saving capabilities on this entry
       variables = ImmutableMap.of(
           "entryId", entryId
       );
@@ -288,57 +252,15 @@ public class GUIHandler {
       return GSON.toJson(variables);
     }
   }
-//
-//  public static class HandleClickSaveButton implements Route {
-//    private static final Gson GSON = new Gson();
-//    // Saves the last response list, entry tags created here
-//    /**
-//     * Handles Axios requests from the javascript front-end and returns
-//     * the appropriate JSON object to be used by the front-end.
-//     *
-//     * @param request - request object for Axios request
-//     * @param response - response object for Axios request
-//     * @return a JSON object representing information to be used by the front end
-//     * @throws Exception if data cannot be accessed from given JSON object
-//     */
-//    @Override
-//    public Object handle(Request request, Response response) throws Exception {
-//
-//      JSONObject data = new JSONObject(request.body());
-//      Map<String, Object> variables;
-//
-//      Integer entryId = Integer.parseInt(data.getString("entryID"));
-//      String userNameOrUserID = data.getString("userID");
-//      String text = data.getString("text");
-//      // probably will not even need this, ideally from backend should be able to detect
-//      // the most recently saved entry and get that from SQL using a query
-//      WordCountVec vectorizor = new WordCountVec();
-//      variables = vectorizor.parseToGui();
-//      /*-------*/
-//      /*
-//      Currently there is only a method to create a new entry row in the entries table.
-//      If we want to continually update a given entry, we would have to use its id to retrieve
-//      the specific entry.
-//
-//      One way we can do this is by having separate handlers/methods/etc. for starting an entry
-//      and saving in an entry. For instance, on creation of an entry we would query the database
-//      to find the new entry's id and pass that to the frontend. Then, whenever we save the entry,
-//      we can pass the entry id as well so that Java knows which entry to update.
-//
-//      Alternatively, if we ensure that each entry has a unique date, we could
-//      request and update a given entry that way.
-//       */
-//      /*-------*/
-//      return GSON.toJson(variables);
-//    }
-//  }
 
+  /**
+   * Class that manages all requests for a user's history summary.
+   */
   public static class HandleRequestUserHistorySummary implements Route {
     private static final Gson GSON = new Gson();
 
     /**
-     * Handles Axios requests from the javascript front-end and returns
-     * the appropriate JSON object to be used by the front-end.
+     * Handle request for a user's entry history.
      *
      * @param request  - request object for Axios request
      * @param response - response object for Axios request
@@ -351,35 +273,26 @@ public class GUIHandler {
       JSONObject data = new JSONObject(request.body());
       Map<String, Object> variables;
 
-      String userNameOrUserID = data.getString("token");
+      String username = data.getString("token");
 
       JSONArray entriesMaps
-          = BackendConnection.getEntriesSummaryFromUsername(userNameOrUserID);
+          = BackendConnection.getEntriesSummaryFromUsername(username);
 
       variables = ImmutableMap.of(
           "entries", entriesMaps);
-
-      /*-------*/
-      /*
-      You can get all of the entries from a user using getUserEntriesByUsername in JournalTexterDB.
-      This will form instances of the Entry class from the string representation that you can work
-      with.
-
-      I can update the SQL database to have a new table that holds tags that relate to each entry
-      so they can easily be retrieved.
-       */
-      /*-------*/
 
       return GSON.toJson(variables);
     }
   }
 
+  /**
+   * Class that manages all requests for a specific entry's contents.
+   */
   public static class HandleRequestUserSpecificHistory implements Route {
     private static final Gson GSON = new Gson();
 
     /**
-     * Handles Axios requests from the javascript front-end and returns
-     * the appropriate JSON object to be used by the front-end.
+     * Handles getting all data from a specific user entry.
      *
      * @param request  - request object for Axios request
      * @param response - response object for Axios request
@@ -393,7 +306,6 @@ public class GUIHandler {
       Map<String, Object> variables;
 
       Integer entryId = Integer.parseInt(data.getString("entryID"));
-      String userNameOrUserID = data.getString("userID");
 
       JournalTexterDB jtDB = JournalTexterDB.getInstance();
 
@@ -403,6 +315,7 @@ public class GUIHandler {
       List<List<String>> accumulatedResponses = new ArrayList<>();
       List<String> responsesForQuestion = new ArrayList<>();
 
+      // separate out the questions and responses
       for (JournalText jt : entry.getQuestionsAndResponses()) {
         if (jt.getType() == JournalTextType.QUESTION) {
           questions.add(jt.getText());
@@ -420,14 +333,6 @@ public class GUIHandler {
           "tags", entry.getTags(),
           "sentiment", entry.getSentiment());
 
-      /*-------*/
-      /*
-      Assuming this relates to seeing all the text of the entry after a user pulls it up, this can
-      be done by sending the id of the entry. I can create a method to retrieve the entry based
-      on its id, or all entries from a given date.
-       */
-      /*-------*/
-
       return GSON.toJson(variables);
     }
   }
@@ -441,8 +346,7 @@ public class GUIHandler {
     private static final Gson GSON = new Gson();
 
     /**
-     * Handles Axios requests from the javascript front-end and returns
-     * the appropriate JSON object to be used by the front-end.
+     * TODO
      *
      * @param request  - request object for Axios request
      * @param response - response object for Axios request
@@ -483,8 +387,7 @@ public class GUIHandler {
     private static final Gson GSON = new Gson();
 
     /**
-     * Handles Axios requests from the javascript front-end and returns
-     * the appropriate JSON object to be used by the front-end.
+     * TODO
      *
      * @param request  - request object for Axios request
      * @param response - response object for Axios request
